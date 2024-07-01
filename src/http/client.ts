ee4add3cbe71e1ebd0225ebf4d338a95a4ef16c1
= <yashbansodgit@gmail.com>
2024-07-02 00:05:54 +0530

    feat: axios intercepter

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "@/store/auth";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
@@ -8,3 +9,35 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+const refreshTokenRequest = async () => {
+  const { data } = await api.get(
+    `${import.meta.env.VITE_BACKEND_API_URL}/api/auth/refresh`,
+    { withCredentials: true },
+  );
+  return data;
+};
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const headers = { ...originalRequest.headers };
+        await refreshTokenRequest();
+        return api.request({ ...originalRequest, headers });
+      } catch (error: unknown) {
+        useAuthStore.getState().removeUser();
+        const errorMessage =
+          (error as Error).message ?? "Error while refreshing token";
+        return Promise.reject(new Error(errorMessage));
+      }
+    }
+    return Promise.reject(new Error(error.message));
+  },
+);
--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { self } from "@/http/api";
 import { useAuthStore } from "@/store/auth";
+import { AxiosError } from "axios";
 
 const handleGetSelfDetails = async () => {
   const { data } = await self();
@@ -14,6 +15,15 @@ export const RootLayout = () => {
   const { data, isLoading } = useQuery({
     queryKey: ["auth/self"],
     queryFn: handleGetSelfDetails,
+    retry: (failureCount: number, error) => {
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        return false;
+      }
+      if (failureCount < 3) {
+        return true;
+      }
+      return false;
+    },
   });
 
   React.useEffect(() => {
