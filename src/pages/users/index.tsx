c6d6cc60617fec15407c5311e5600e7c794523dd
= <yashbansodgit@gmail.com>
2024-07-06 23:44:54 +0530

    feat: add Breadcrumb

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,17 @@
+import { Breadcrumb } from "antd"
+import { Link } from "react-router-dom";
+import { RightOutlined } from "@ant-design/icons";
+
 export const UsersPage = () => {
-  return <h1>Users Page</h1>;
+  return (
+    <div>
+      <Breadcrumb
+        separator={<RightOutlined />}
+        items={[
+          { title: <Link to="/">Dashboard</Link> },
+          { title: 'Users' }
+        ]}
+      />;
+    </div>
+  )
 };
